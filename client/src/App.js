c89817ce6a5a4124813e4a7492cf7017d9e8fbd9
Jeffrey XIE <xwy5201314@gmail.com>
2019-03-07 16:14:38 -0500

    Change to trigger redeployment

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <Link to="/">Home2</Link>
+            <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
